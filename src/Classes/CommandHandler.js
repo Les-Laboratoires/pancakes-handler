d4ef56bb1e1d26291fc5c88362c95c0e2889d70f
loockeeer <lulu.r1313@gmail.com>
2020-01-20 18:41:22 +0100

    Removed private props and started reading commands

--- src/Classes/CommandHandler.js
+++ src/Classes/CommandHandler.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js')
+const readRecursive = require('../Tools/readRecursive.js')
+
 class CommandHandler {
+
   constructor(config, clientOptions) {
-    this.#client = new Discord.Client(clientOptions)
-    this.#config = config
-    this.#commands = new CommandManager()
-    this.#events = new EventManager()
+    this._client = new Discord.Client(clientOptions)
+    this._config = config
+    this._commands = new CommandManager()
+    this._events = new EventManager()
   }
   launch(token) {
-    this.#client.login(token)
+    this._client.login(token)
+    readRecursive(this._config.commandsFolder).then(files => {
+
+    })
     // Handle commands and events
   }
 }
