e40da559063c7d2178680f93d8859fd3df87f0d3
loockeeer <lulu.r1313@gmail.com>
2020-01-22 14:25:17 +0100

    Edit command exemple with preprocessors for more possibilities

--- tests/commands/ping.js
+++ tests/commands/ping.js
@@ -1,15 +1,31 @@
 const Command = require('../../src/Classes/Command.js')
-class PingCommand extends Command {
+module.exports = class PingCommand extends Command { // Extends of Command
   constructor() {
-    super({
-      name: "ping",
-      aliases: ['pingu'],
-      permissions: ["BAN_MEMBERS", "|", "KICK_MEMBERS"],
-      args: []
+    super({ // Passing command arguments
+      name: "ping", // Set command's name
+
+      aliases: ['pingu'], // Set command's aliases
+
+      permissions: ["BAN_MEMBERS", "|", "KICK_MEMBERS"], // Preprocessor
+
+      args: [ // Set args for command
+        {
+          name: "user", // Arg name
+
+          value: "preprocessors.custom.getUser", // Arg value checker (preprocessor)
+
+          required: true // Whether the arg is required or not
+          // Custom args for preprocessor
+        }
+      ]
+      preprocessors: [ // Load preprocessor only for this command
+        'preprocessors.custom.onlyModerator'
+      ]
+      // Custom args for Preprocessors
+
     })
   }
-  run(client, message, args) {
+  run(client, message, args) { // Execute command
     message.reply('Pong !')
   }
 }
-module.exports = PingCommand
