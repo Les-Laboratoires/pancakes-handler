9879ef9231f7f78bb9d42304ffada21ca266944b
loockeeer <lulu.r1313@gmail.com>
2020-01-24 17:46:24 +0100

    add pancakes module

--- src/classes/command/Command.js
+++ src/classes/command/Command.js
@@ -1,14 +1,17 @@
-module.exports = class Command {
+const PancakesModule = require('../PancakesModule.js')
+module.exports = class Command extends PancakesModule {
   constructor(props) {
+    super(props.instance)
     this.props = props
+
     // Checks
     if (!this.props.name) return Error('Name is a required property.')
-    if (this.props.instance._commands.getCommand(this.props.name)) return Error(`Name ${this.props.name} is already used`)
+    if (this._instance._commands.getCommand(this.props.name)) return Error(`Name ${this.props.name} is already used`)
     if (typeof this.props.name !== "string") return Error("Name must be of type string")
 
     if (this.props.aliases && typeof this.props.aliases !== "array") return Error('Aliases property must be of type Array')
     else if (this.props.aliases && this.props.aliases.every(alias => typeof alias !== "string" || !alias)) return Error('Aliases must be of type String and not null')
-    if (!this.props.aliases.every(alias => !this.props.instance.getCommand(alias))) return Error('Aliases must not be name of commands or other aliases')
+    if (!this.props.aliases.every(alias => !this_.instance.getCommand(alias))) return Error('Aliases must not be name of commands or other aliases')
 
     if (this.props.args && typeof this.props.args !== "array") return Error('Args property must be of type Array')
     else if (this.props.args && this.props.args.every(arg => typeof arg !== "object")) return Error('Args must be of type Object')
--- src/classes/event/Event.js
+++ src/classes/event/Event.js
@@ -1,9 +1,7 @@
-module.exports = class Event {
+const PancakesModule = require('../PancakesModule.js')
+module.exports = class Event extends PancakesModule {
   constructor(props) {
-    this.props = {
-      ...props,
-      filepath: __filename
-    }
+    super(props.instance)
     // Checks
     if (!this.props.name) return Error('Event name is a required property')
   }
